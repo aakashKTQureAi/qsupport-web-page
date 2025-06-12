1ed19db4ec339a6a078c3e89681453bfc65f5877
ktaakash <ktaakash@gmail.com>
2025-06-12 15:57:56 +0530

    pyhthonanywhere conffig addition

--- welcome_project/settings.py
+++ welcome_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_@p8pb8&)z7)!gke97c6cxm*3vp)cbc2s*7q7-xk-2f85-=r--'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+ALLOWED_HOSTS = ['your-username.pythonanywhere.com']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
