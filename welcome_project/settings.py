986ed0b6755486b9cff5eb1117b932f9ce751450
ktaakash <ktaakash@gmail.com>
2025-06-12 16:23:49 +0530

    pyhthonanywhere conffig addition

--- welcome_project/settings.py
+++ welcome_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_@p8pb8&)z7)!gke97c6cxm*3vp)cbc2s*7q7-xk-2f85-=r--
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['your-username.pythonanywhere.com']
+ALLOWED_HOSTS = ['aakashkt.pythonanywhere.com']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
